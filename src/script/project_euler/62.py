e6fa6caee80e9ef14452b4e9670a295fb5094860
Naresh Shenoy <nareshenoy@yahoo.com>
2014-01-26 22:12:33 +0530

    o Add a new directory for topcoder. o Solution to problem 62 on project euler

---
+++ src/script/project_euler/62.py
@@ -0,0 +1,6 @@
+def is_cube(n):
+    cube_rt = n ** (1/3.0)
+    return int(cube_rt) == cube_rt
+
+def all_permutations(n):
+    digits = 
---
+++ src/topcoder/srm/144_div_1_800.py
@@ -0,0 +1,10 @@
+# Roundabout problem
+class Roundabout(object):
+    def clearUpTime(self, north, east, south, west):
+        """
+        Returns an integer number of seconds it takes 
+        to empty the roundabout.
+
+        """
+        total_time = 0
+
