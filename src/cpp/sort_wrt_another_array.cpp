fb94e770cd75d0b619a145397eff368424ec84ed
Naresh Shenoy <nareshenoy@yahoo.com>
2014-08-14 01:47:48 +0530

    Add some code to get a sorted array depending on the ordering in another array

--- src/cpp/sort_wrt_another_array.cpp
+++ src/cpp/sort_wrt_another_array.cpp
@@ -5,11 +5,48 @@ http://www.geeksforgeeks.org/sort-array-according-order-defined-another-array/
 */
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include "sorting.h"
+
 using namespace std;
 
-void sort_wrt_array(vector<int>& a1, vector<int>& a2) {
-    return;
+vector<int>& sort_wrt_array(vector<int>& a1, vector<int>& a2) {
+    unordered_map <int, int> num_to_occurrence;
+    vector<int> elems_not_in_a2;
+    for(int i = 0;i < a1.size(); i++) {
+        if(num_to_occurrence.find(a1[i]) == num_to_occurrence.end()) {
+            num_to_occurrence[a1[i]] = 1;
+        }
+        else {
+            num_to_occurrence[a1[i]]++;
+        }
+    }
+    int ind = 0;
+    for(int i = 0;i < a2.size();i++) {
+        if(num_to_occurrence.find(a2[i]) != num_to_occurrence.end()) {
+            for(int j = 0;j < num_to_occurrence[a2[i]];j++) {
+                a1[ind] = a2[i];
+                ind = ind + 1;
+            }
+            num_to_occurrence.erase(a2[i]);
+        }
+    }
+    // Now all elements left in num_to_occurrence are not present in a2
+    // we must sort them 
+    for(unordered_map<int, int>::iterator itr = num_to_occurrence.begin(); itr != num_to_occurrence.end(); itr++ ) {
+        for(int i = 0;i < itr->second;i++) {
+            elems_not_in_a2.push_back(itr->first);
+        }
+    }
+    elems_not_in_a2 = merge_sort(elems_not_in_a2);
+
+    for(int i = 0;i < elems_not_in_a2.size(); i++) {
+        a1[ind] = elems_not_in_a2[i];
+        ind = ind + 1;
+    }
+    return a1;
 }
+
 int main() {
 
     int n, k;
@@ -30,7 +67,10 @@ int main() {
         a2.push_back(tmp);
     }
 
-    sort_wrt_array(a1, a2);
-
+    vector<int> sorted_arr = sort_wrt_array(a1, a2);
+    for(int i = 0;i < sorted_arr.size(); i++) {
+        cout << sorted_arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
