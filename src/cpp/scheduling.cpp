2fead3a87893dc73561e21448c8ed825dedd1c1d
Naresh Shenoy <nareshenoy@yahoo.com>
2014-08-21 23:26:50 +0530

    Skeleton for the scheduling problem.
    Details at: http://www.cseblog.com/2012/05/scheduling-problem.html

---
+++ src/cpp/scheduling.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print_schedule(int nsets, int njobs) {
+    return;
+}
+
+int main() {
+    int n;
+    cout << "Please enter the number of sets: ";
+    cin >> n;
+
+    int m;
+    cout << "Please enter the number of jobs in each set: ";
+    cin >> m;
+
+    print_schedule(n, m);
+
+    return 0;
+}
+
